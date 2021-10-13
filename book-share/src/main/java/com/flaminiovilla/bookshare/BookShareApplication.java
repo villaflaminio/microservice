4159caa2540fcc0d224cf09247a443f818f7bca4
villaflaminio <vifla01@gmail.com>
2021-10-13 11:51:48 +0200

    eureka client funzionante !

--- book-share/src/main/java/com/flaminiovilla/bookshare/BookShareApplication.java
+++ book-share/src/main/java/com/flaminiovilla/bookshare/BookShareApplication.java
@@ -4,12 +4,16 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Log4j2
 @EnableSwagger2
 @SpringBootApplication
+@EnableEurekaClient
 public class BookShareApplication {
 
     public static void main(String[] args) {
